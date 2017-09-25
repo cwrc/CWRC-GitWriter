c24a24c6641e2785338b10bd7dc1193160b5a157
Andrew <ajmacdonald@users.noreply.github.com>
2017-09-25 17:47:05 -0400

    require changes

--- src/js/app.js
+++ src/js/app.js
@@ -10,6 +10,7 @@ require('./jquery/plugins/jquery.watermark.min.js')
 require('./jquery/plugins/jquery.xpath.js')
 require('./jquery/plugins/jquery.popup.js')
 
+require('jquery-ui/ui/widgets/resizable')
 require('jquery-ui/ui/widgets/tabs')
 require('jquery-ui/ui/widgets/checkboxradio')
 require('jquery-ui/ui/widgets/button')
@@ -17,11 +18,4 @@ require('jquery-ui/ui/widgets/controlgroup')
 require('jquery-ui/ui/widgets/dialog')
 require('jquery-ui/ui/widgets/tooltip')
 
-var CWRCWriter = require('cwrc-writer-base')
-var config = require('./config');
-//  config.storageDialogs = storageDialogs;
-//config.entityLookupDialogs = require('cwrc-public-entity-dialogs');
-//config.layout = require('./layout-config');
-
-var writer = new CWRCWriter(config);
-window.Drupal.CWRCWriter.writer = writer;
+window.Drupal.CWRCWriter = require('cwrc-writer-base')
