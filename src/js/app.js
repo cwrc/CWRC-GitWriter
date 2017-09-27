5737a96e5c1b309e253f6d82180d55559d4e5ed4
Andrew <ajmacdonald@users.noreply.github.com>
2017-09-27 15:17:02 -0400

    added storage dialogs

--- src/js/app.js
+++ src/js/app.js
@@ -3,5 +3,8 @@ if (!window.$) {
 	window.jQuery = window.$ = require('jquery');
 }
 
+window.Drupal.CWRCWriterConfig = require('./config.js');
 window.Drupal.CWRCWriter = require('cwrc-writer-base');
 window.Drupal.CWRCWriterLayout = require('./layout-config.js');
+window.Drupal.CWRCWriterDialogs = require('cwrc-dialogs');
+window.Drupal.CWRCWriterStorageDialogs = require('./storage-dialogs.js');
