f73b15951e6623185a630b4a32011ee888164de6
Andrew <ajmacdonald@users.noreply.github.com>
2017-11-06 18:25:00 -0500

    change require order
    jquery-ui needs to load after bootstrap

--- src/js/app.js
+++ src/js/app.js
@@ -2,8 +2,8 @@ if (!window.$) {
 	window.jQuery = window.$ = require('jquery');
 }
 
-window.CWRCWriterConfig = require('./config.js');
-window.CWRCWriter = require('cwrc-writer-base');
 window.CWRCWriterLayout = require('./layout-config.js');
 window.CWRCWriterDialogs = require('cwrc-public-entity-dialogs');
 window.CWRCWriterStorageDialogs = require('./storage-dialogs.js');
+window.CWRCWriterConfig = require('./config.js');
+window.CWRCWriter = require('cwrc-writer-base');
