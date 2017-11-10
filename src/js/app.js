fe32cbdf62b38f4271c11df5d0f7f918e46d7c0f
James Chartrand <jc.chartrand@gmail.com>
2017-11-10 10:38:23 -0500

    feat: add git dialogs and new entity lookup dialogs

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,26 @@
 if (!window.$) {
-	window.jQuery = window.$ = require('jquery');
+    window.jQuery = window.$ = require('jquery');
 }
 
-window.CWRCWriterLayout = require('./layout-config.js');
-window.CWRCWriterDialogs = require('cwrc-public-entity-dialogs');
-window.CWRCWriterStorageDialogs = require('./storage-dialogs.js');
-window.CWRCWriterConfig = require('./config.js');
-window.CWRCWriter = require('cwrc-writer-base');
+window.CWRCWriterStorageDialogs = require('cwrc-git-dialogs');
+
+// only continue loading the cwrcWriter if the user has authenticated with github
+if (window.CWRCWriterStorageDialogs.authenticate()) {
+
+    window.CWRCWriterLayout = require('./layout-config.js');
+    let viaf = require('viaf-entity-lookup')
+    let dbpedia = require('dbpedia-entity-lookup');
+    let wikidata = require('wikidata-entity-lookup');
+    let getty = require('getty-entity-lookup');
+    window.CWRCWriterDialogs = require('cwrc-public-entity-dialogs');
+
+    window.CWRCWriterDialogs.registerEntitySources({
+        people: (new Map()).set('viaf', viaf).set('wikidata', wikidata).set('getty', getty).set('dbpedia', dbpedia),
+        places: (new Map()).set('viaf', viaf).set('dbpedia', dbpedia).set('wikidata', wikidata),
+        organizations: (new Map()).set('viaf', viaf).set('wikidata', wikidata).set('dbpedia', dbpedia),
+        titles: (new Map()).set('viaf', viaf).set('wikidata', wikidata).set('dbpedia', dbpedia),
+    })
+    //window.CWRCWriterStorageDialogs = require('./storage-dialogs.js');
+    window.CWRCWriterConfig = require('./config.js');
+    window.CWRCWriter = require('cwrc-writer-base');
+}
