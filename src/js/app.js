86b1c1d26f8d82c7f3bc6d5c664d5bead6b06446
Andrew <ajmacdonald@users.noreply.github.com>
2018-01-31 11:53:06 -0500

    move init to app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,33 @@
-window.CWRCWriterStorageDialogs = require('cwrc-git-dialogs');
+let CWRCWriterStorageDialogs = require('cwrc-git-dialogs');
 
 // only continue loading the cwrcWriter if the user has authenticated with github
-if (window.CWRCWriterStorageDialogs.authenticate()) {
+if (CWRCWriterStorageDialogs.authenticate()) {
 
     let viaf = require('viaf-entity-lookup')
     let dbpedia = require('dbpedia-entity-lookup');
     let wikidata = require('wikidata-entity-lookup');
     let getty = require('getty-entity-lookup');
-    window.CWRCWriterDialogs = require('cwrc-public-entity-dialogs');
+    let CWRCWriterDialogs = require('cwrc-public-entity-dialogs');
 
-    window.CWRCWriterDialogs.registerEntitySources({
+    CWRCWriterDialogs.registerEntitySources({
         people: (new Map()).set('viaf', viaf).set('wikidata', wikidata).set('getty', getty).set('dbpedia', dbpedia),
         places: (new Map()).set('viaf', viaf).set('dbpedia', dbpedia).set('wikidata', wikidata),
         organizations: (new Map()).set('viaf', viaf).set('wikidata', wikidata).set('dbpedia', dbpedia),
         titles: (new Map()).set('viaf', viaf).set('wikidata', wikidata).set('dbpedia', dbpedia),
     })
     //window.CWRCWriterStorageDialogs = require('./storage-dialogs.js');
-    window.CWRCWriterConfig = require('./config.js');
-    window.CWRCWriter = require('cwrc-writer-base');
+    let config = require('./config.js');
+    config.container = 'cwrcWriterContainer';
+    config.modules = {
+        west: [ 'structure', 'entities' ],
+        south: [ 'selection', 'validation' ]
+    };
+    config.entityLookupDialogs = CWRCWriterDialogs;
+    config.storageDialogs = CWRCWriterStorageDialogs;
+    
+    let CWRCWriter = require('cwrc-writer-base');
+    var writer = new CWRCWriter(config);
+    writer.event('writerInitialized').subscribe(function() {
+        writer.showLoadDialog();
+    });
 }
