7ae6895f1bab13b0b718651d8fbdfc9116dd9d80
Andrew <ajmacdonald@users.noreply.github.com>
2018-02-20 12:58:09 -0500

    add relations tab, remove extra comma

--- src/js/app.js
+++ src/js/app.js
@@ -14,13 +14,13 @@ if (CWRCWriterStorageDialogs.authenticate()) {
         people: (new Map()).set('viaf', viaf).set('wikidata', wikidata).set('getty', getty).set('dbpedia', dbpedia),
         places: (new Map()).set('geonames', geonames).set('viaf', viaf).set('dbpedia', dbpedia).set('wikidata', wikidata),
         organizations: (new Map()).set('viaf', viaf).set('wikidata', wikidata).set('dbpedia', dbpedia),
-        titles: (new Map()).set('viaf', viaf).set('wikidata', wikidata).set('dbpedia', dbpedia),
+        titles: (new Map()).set('viaf', viaf).set('wikidata', wikidata).set('dbpedia', dbpedia)
     })
     //window.CWRCWriterStorageDialogs = require('./storage-dialogs.js');
     let config = require('./config.js');
     config.container = 'cwrcWriterContainer';
     config.modules = {
-        west: [ 'structure', 'entities' ],
+        west: [ 'structure', 'entities', 'relations' ],
         south: [ 'selection', 'validation' ],
         east: [ 'imageViewer' ]
     };
