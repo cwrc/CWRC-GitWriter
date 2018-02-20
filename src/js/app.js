b2d575a2cdf7fc3084ea2c5a4298101eca5c9b15
Andrew <ajmacdonald@users.noreply.github.com>
2018-02-20 13:14:22 -0500

    update dependencies, add geonames and imageViewer

--- src/js/app.js
+++ src/js/app.js
@@ -7,20 +7,22 @@ if (CWRCWriterStorageDialogs.authenticate()) {
     let dbpedia = require('dbpedia-entity-lookup');
     let wikidata = require('wikidata-entity-lookup');
     let getty = require('getty-entity-lookup');
+    let geonames = require('geonames-entity-lookup');
     let CWRCWriterDialogs = require('cwrc-public-entity-dialogs');
 
     CWRCWriterDialogs.registerEntitySources({
         people: (new Map()).set('viaf', viaf).set('wikidata', wikidata).set('getty', getty).set('dbpedia', dbpedia),
-        places: (new Map()).set('viaf', viaf).set('dbpedia', dbpedia).set('wikidata', wikidata),
+        places: (new Map()).set('geonames', geonames).set('viaf', viaf).set('dbpedia', dbpedia).set('wikidata', wikidata),
         organizations: (new Map()).set('viaf', viaf).set('wikidata', wikidata).set('dbpedia', dbpedia),
-        titles: (new Map()).set('viaf', viaf).set('wikidata', wikidata).set('dbpedia', dbpedia),
+        titles: (new Map()).set('viaf', viaf).set('wikidata', wikidata).set('dbpedia', dbpedia)
     })
     //window.CWRCWriterStorageDialogs = require('./storage-dialogs.js');
     let config = require('./config.js');
     config.container = 'cwrcWriterContainer';
     config.modules = {
-        west: [ 'structure', 'entities' ],
-        south: [ 'selection', 'validation' ]
+        west: [ 'structure', 'entities', 'relations' ],
+        south: [ 'selection', 'validation' ],
+        east: [ 'imageViewer' ]
     };
     config.entityLookupDialogs = CWRCWriterDialogs;
     config.storageDialogs = CWRCWriterStorageDialogs;
