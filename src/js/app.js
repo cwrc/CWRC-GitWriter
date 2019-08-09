58b73c795d18160018530ca92592b04963fb72af
Andrew <ajmacdonald@users.noreply.github.com>
2019-08-09 16:37:50 -0400

    fix: update dependency, remove writer.exit override

--- src/js/app.js
+++ src/js/app.js
@@ -46,10 +46,6 @@ document.addEventListener('DOMContentLoaded', function() {
     let CWRCWriter = require('cwrc-writer-base');
     var writer = new CWRCWriter(config);
 
-    writer.exit = function() {
-        writer.storageDialogs.logOut();
-    }
-
     window.writer = writer;
 
     writer.event('writerInitialized').subscribe(function() {
