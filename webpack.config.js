71f4a415d10ef1457947bc2c69934422f8faef04
Andrew <ajmacdonald@users.noreply.github.com>
2019-08-30 14:32:05 -0400

    fix: update dependencies, webpack tweaks

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,9 @@
 const path = require('path');
 const webpack = require('webpack');
-
-/*
- * SplitChunksPlugin is enabled by default and replaced
- * deprecated CommonsChunkPlugin. It automatically identifies modules which
- * should be splitted of chunk by heuristics using module duplication count and
- * module category (i. e. node_modules). And splits the chunks…
- *
- * It is safe to remove "splitChunks" from the generated configuration
- * and was added as an educational example.
- *
- * https://webpack.js.org/plugins/split-chunks-plugin/
- *
- */
-
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
-/*
- * We've enabled HtmlWebpackPlugin for you! This generates a html
- * page for you when you compile webpack, which will make you start
- * developing and prototyping faster.
- *
- * https://github.com/jantimon/html-webpack-plugin
- *
- */
-
-module.exports = {
-	mode: 'development',
+let config = {
 	entry: './src/js/app.js',
 
 	output: {
@@ -53,7 +30,7 @@ module.exports = {
 						],
 						plugins: [
 							"@babel/plugin-proposal-class-properties",
-							["@babel/plugin-transform-runtime",{
+							["@babel/plugin-transform-runtime", {
 								"absoluteRuntime": false,
 								"corejs": false,
 								"helpers": false,
@@ -73,19 +50,15 @@ module.exports = {
 	},
 
 	// optimization: {
-	// 	splitChunks: {
-	// 		cacheGroups: {
-	// 			vendors: {
-	// 				priority: -10,
-	// 				test: /[\\/]node_modules[\\/]/
+	// 	minimizer: [
+	// 		new UglifyJsPlugin({
+	// 			uglifyOptions: {
+	// 				output: {
+	// 					comments: false
+	// 				}
 	// 			}
-	// 		},
-
-	// 		chunks: 'async',
-	// 		minChunks: 1,
-	// 		minSize: 30000,
-	// 		name: true
-	// 	}
+	// 		})
+	// 	]
 	// },
 
 	node: {
@@ -96,4 +69,16 @@ module.exports = {
 		open: false,
 		contentBase: './build'
 	}
+};
+
+module.exports = (env, argv) => {
+	if (argv.mode === 'development') {
+		config.devtool = 'eval-source-map';
+	}
+
+	if (argv.mode === 'production') {
+		config.devtool = 'source-map';
+	}
+
+	return config;
 };
