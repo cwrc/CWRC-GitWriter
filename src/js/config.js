e4196ecf1c28b3718d7801d5c54adfe51f789ae3
Luciano Frizzera <lucaju@me.com>
2019-11-28 14:28:42 -0500

    fix(config.js): Change Schema Config to list url as arrays
    BREAKING CHANGE: Change Schema XML and CSS loading functions

--- src/js/config.js
+++ src/js/config.js
@@ -7,102 +7,150 @@ module.exports = {
         schemas: [{
                 id: 'tei',
                 name: 'CWRC Basic TEI Schema',
-                url: 'https://cwrc.ca/schemas/cwrc_tei_lite.rng',
-                altUrl: 'https://raw.githubusercontent.com/cwrc/CWRC-Schema/master/schemas/cwrc_tei_lite.rng',
-                cssUrl: 'https://cwrc.ca/templates/css/tei.css',
-                altCssUrl: 'https://raw.githubusercontent.com/cwrc/CWRC-Schema/master/templates/css/tei.css',
-                schemaMappingsId: 'tei'
+                schemaMappingsId: 'tei',
+                xmlUrl: [
+                    'https://cwrc.ca/schemas/cwrc_tei_lite.rng',
+                    'https://raw.githubusercontent.com/cwrc/CWRC-Schema/master/schemas/cwrc_tei_lite.rng'
+                ],
+                cssUrl: [
+                    'https://cwrc.ca/templates/css/tei.css',
+                    'https://raw.githubusercontent.com/cwrc/CWRC-Schema/master/templates/css/tei.css'
+                ]
             },
             {
                 id: 'events',
                 name: 'Events Schema',
-                url: 'https://cwrc.ca/schemas/orlando_event_v2.rng',
-                altUrl: 'https://raw.githubusercontent.com/cwrc/CWRC-Schema/master/schemas/orlando_event_v2.rng',
-                cssUrl: 'https://cwrc.ca/templates/css/orlando_v2_cwrc-writer.css',
-                altCssUrl: 'https://raw.githubusercontent.com/cwrc/CWRC-Schema/master/templates/css/orlando_v2_cwrc-writer.css',
-                schemaMappingsId: 'orlando'
+                schemaMappingsId: 'orlando',
+                xmlUrl: [
+                    'https://cwrc.ca/schemas/orlando_event_v2.rng',
+                    'https://raw.githubusercontent.com/cwrc/CWRC-Schema/master/schemas/orlando_event_v2.rng'
+                ],
+                cssUrl: [
+                    'https://cwrc.ca/templates/css/orlando_v2_cwrc-writer.css',
+                    'https://raw.githubusercontent.com/cwrc/CWRC-Schema/master/templates/css/orlando_v2_cwrc-writer.css'
+                ]
             },
             {
                 id: 'biography',
                 name: 'Biography Schema',
-                url: 'https://cwrc.ca/schemas/orlando_biography_v2.rng',
-                altUrl: 'https://raw.githubusercontent.com/cwrc/CWRC-Schema/master/schemas/orlando_biography_v2.rng',
-                cssUrl: 'https://cwrc.ca/templates/css/orlando_v2_cwrc-writer.css',
-                altCssUrl: 'https://raw.githubusercontent.com/cwrc/CWRC-Schema/master/templates/css/orlando_v2_cwrc-writer.css',
-                schemaMappingsId: 'orlando'
+                schemaMappingsId: 'orlando',
+                xmlUrl: [
+                    'https://cwrc.ca/schemas/orlando_biography_v2.rng',
+                    'https://raw.githubusercontent.com/cwrc/CWRC-Schema/master/schemas/orlando_biography_v2.rng'
+                ],
+                cssUrl: [
+                    'https://cwrc.ca/templates/css/orlando_v2_cwrc-writer.css',
+                    'https://raw.githubusercontent.com/cwrc/CWRC-Schema/master/templates/css/orlando_v2_cwrc-writer.css'
+                ]
             },
             {
                 id: 'writing',
                 name: 'Writing Schema',
-                url: 'https://cwrc.ca/schemas/orlando_writing_v2.rng',
-                altUrl: 'https://raw.githubusercontent.com/cwrc/CWRC-Schema/master/schemas/orlando_writing_v2.rng',
-                cssUrl: 'https://cwrc.ca/templates/css/orlando_v2_cwrc-writer.css',
-                altCssUrl: 'https://raw.githubusercontent.com/cwrc/CWRC-Schema/master/templates/css/orlando_v2_cwrc-writer.css',
-                schemaMappingsId: 'orlando'
+                schemaMappingsId: 'orlando',
+                xmlUrl: [
+                    'https://cwrc.ca/schemas/orlando_writing_v2.rng',
+                    'https://raw.githubusercontent.com/cwrc/CWRC-Schema/master/schemas/orlando_writing_v2.rng'
+                ],
+                cssUrl: [
+                    'https://cwrc.ca/templates/css/orlando_v2_cwrc-writer.css',
+                    'https://raw.githubusercontent.com/cwrc/CWRC-Schema/master/templates/css/orlando_v2_cwrc-writer.css'
+                ]
             },
             {
                 id: 'cwrcEntry',
                 name: 'CWRC Entry Schema',
-                url: 'https://cwrc.ca/schemas/cwrc_entry.rng',
-                altUrl: 'https://raw.githubusercontent.com/cwrc/CWRC-Schema/master/schemas/cwrc_entry.rng',
-                cssUrl: 'https://cwrc.ca/templates/css/cwrc.css',
-                altCssUrl: 'https://raw.githubusercontent.com/cwrc/CWRC-Schema/master/templates/css/cwrc.css',
-                schemaMappingsId: 'cwrcEntry'
+                schemaMappingsId: 'cwrcEntry',
+                xmlUrl: [
+                    'https://cwrc.ca/schemas/cwrc_entry.rng',
+                    'https://raw.githubusercontent.com/cwrc/CWRC-Schema/master/schemas/cwrc_entry.rng'
+                ],
+                cssUrl: [
+                    'https://cwrc.ca/templates/css/cwrc.css',
+                    'https://raw.githubusercontent.com/cwrc/CWRC-Schema/master/templates/css/cwrc.css'
+                ]
             },
             {
                 id: 'epidoc',
                 name: 'EpiDoc Schema',
-                url: 'http://www.stoa.org/epidoc/schema/latest/tei-epidoc.rng',
-                altUrl: 'https://cwrc.ca/epidoc/schema/latest/tei-epidoc.rng',
-                cssUrl: 'https://cwrc.ca/templates/css/tei.css',
-                schemaMappingsId: 'tei'
+                schemaMappingsId: 'tei',
+                xmlUrl: [
+                    'http://www.stoa.org/epidoc/schema/latest/tei-epidoc.rng',
+                    'https://cwrc.ca/epidoc/schema/latest/tei-epidoc.rng'
+                ],
+                cssUrl: [
+                    'https://cwrc.ca/templates/css/tei.css'
+                ]
             },
             {
                 id: 'teiAll',
                 name: 'TEI All Schema',
-                url: 'https://www.tei-c.org/release/xml/tei/custom/schema/relaxng/tei_all.rng',
-                altUrl: 'https://jenkins.tei-c.org/job/TEIP5/lastSuccessfulBuild/artifact/P5/release/xml/tei/custom/schema/relaxng/tei_all.rng',
-                cssUrl: 'https://cwrc.ca/templates/css/tei.css',
-                schemaMappingsId: 'tei'
+                schemaMappingsId: 'tei',
+                xmlUrl: [
+                    'https://www.tei-c.org/release/xml/tei/custom/schema/relaxng/tei_all.rng',
+                    'https://jenkins.tei-c.org/job/TEIP5/lastSuccessfulBuild/artifact/P5/release/xml/tei/custom/schema/relaxng/tei_all.rng'
+                ],
+                cssUrl: [
+                    'https://cwrc.ca/templates/css/tei.css'
+                ]
             },
             {
                 id: 'teiCorpus',
                 name: 'TEI Corpus Schema',
-                url: 'https://www.tei-c.org/release/xml/tei/custom/schema/relaxng/tei_corpus.rng',
-                altUrl: 'https://jenkins.tei-c.org/job/TEIP5/lastSuccessfulBuild/artifact/P5/release/xml/tei/custom/schema/relaxng/tei_corpus.rng',
-                cssUrl: 'https://cwrc.ca/templates/css/tei.css',
-                schemaMappingsId: 'tei'
+                schemaMappingsId: 'tei',
+                xmlUrl: [
+                    'https://www.tei-c.org/release/xml/tei/custom/schema/relaxng/tei_corpus.rng',
+                    'https://jenkins.tei-c.org/job/TEIP5/lastSuccessfulBuild/artifact/P5/release/xml/tei/custom/schema/relaxng/tei_corpus.rng'
+                ],
+                cssUrl: [
+                    'https://cwrc.ca/templates/css/tei.css'
+                ]
             },
             {
                 id: 'teiMs',
                 name: 'TEI Manuscript Schema',
-                url: 'https://www.tei-c.org/release/xml/tei/custom/schema/relaxng/tei_ms.rng',
-                altUrl: 'https://jenkins.tei-c.org/job/TEIP5/lastSuccessfulBuild/artifact/P5/release/xml/tei/custom/schema/relaxng/tei_ms.rng',
-                cssUrl: 'https://cwrc.ca/templates/css/tei.css',
-                schemaMappingsId: 'tei'
+                schemaMappingsId: 'tei',
+                xmlUrl: [
+                    'https://www.tei-c.org/release/xml/tei/custom/schema/relaxng/tei_ms.rng',
+                    'https://jenkins.tei-c.org/job/TEIP5/lastSuccessfulBuild/artifact/P5/release/xml/tei/custom/schema/relaxng/tei_ms.rng'
+                ],
+                cssUrl: [
+                    'https://cwrc.ca/templates/css/tei.css'
+                ]
             },
             {
                 id: 'teiSpeech',
                 name: 'TEI Speech Schema',
-                url: 'https://www.tei-c.org/release/xml/tei/custom/schema/relaxng/tei_speech.rng',
-                altUrl: 'https://jenkins.tei-c.org/job/TEIP5/lastSuccessfulBuild/artifact/P5/release/xml/tei/custom/schema/relaxng/tei_speech.rng',
-                cssUrl: 'https://cwrc.ca/templates/css/tei.css',
-                schemaMappingsId: 'tei'
+                schemaMappingsId: 'tei',
+                xmlUrl: [
+                    'https://www.tei-c.org/release/xml/tei/custom/schema/relaxng/tei_speech.rng',
+                    'https://jenkins.tei-c.org/job/TEIP5/lastSuccessfulBuild/artifact/P5/release/xml/tei/custom/schema/relaxng/tei_speech.rng'
+                ],
+                cssUrl: [
+                    'https://cwrc.ca/templates/css/tei.css'
+                ]
             },
             {
                 id: 'teiLite',
                 name: 'TEI Lite Schema',
-                url: 'https://www.tei-c.org/release/xml/tei/custom/schema/relaxng/tei_lite.rng',
-                altUrl: 'https://jenkins.tei-c.org/job/TEIP5/lastSuccessfulBuild/artifact/P5/release/xml/tei/custom/schema/relaxng/tei_lite.rng',
-                cssUrl: 'https://cwrc.ca/templates/css/tei.css',
-                schemaMappingsId: 'teiLite'
+                schemaMappingsId: 'teiLite',
+                xmlUrl: [
+                    'https://www.tei-c.org/release/xml/tei/custom/schema/relaxng/tei_lite.rng',
+                    'https://jenkins.tei-c.org/job/TEIP5/lastSuccessfulBuild/artifact/P5/release/xml/tei/custom/schema/relaxng/tei_lite.rng',
+                ],
+                cssUrl: [
+                    'https://cwrc.ca/templates/css/tei.css'
+                ]
             },
             {
                 id: 'moravian',
                 name: 'Moravian Lives (TEI)',
-                url: 'https://raw.githubusercontent.com/moravianlives/ML/master/Projects/TEI_Memoirs/out/MoravianMemoirs.rng',
-                cssUrl: 'https://cwrc.ca/templates/css/tei.css',
-                schemaMappingsId: 'teiLite'
+                schemaMappingsId: 'teiLite',
+                xmlUrl: [
+                    'https://raw.githubusercontent.com/moravianlives/ML/master/Projects/TEI_Memoirs/out/MoravianMemoirs.rng'
+                ],
+                cssUrl: [
+                    'https://cwrc.ca/templates/css/tei.css'
+                ]
             }
         ]
     }
