75220e2a2902106cdb76aa99ea41cfa36c9aad8d
Luciano Frizzera <lucaju@me.com>
2020-03-14 19:46:57 -0400

    improvement: update geonames-entity-lookup

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,10 @@
+import geonames from 'geonames-entity-lookup';
 import GitStorageDialogs from 'cwrc-git-dialogs';
 
 const viaf = require('viaf-entity-lookup')
 const dbpedia = require('dbpedia-entity-lookup');
 const wikidata = require('wikidata-entity-lookup');
 const getty = require('getty-entity-lookup');
-const geonames = require('geonames-entity-lookup');
 const lgpn = require('lgpn-entity-lookup');
 const EntityLookupDialogs = require('cwrc-public-entity-dialogs');
 
