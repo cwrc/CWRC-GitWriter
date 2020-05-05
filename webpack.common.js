84efe805354ebae8355224d0580e889973280b97
Luciano Frizzera <lucaju@me.com>
2020-05-05 15:59:11 -0400

    improvement(webpack): add the ability to handle css and less

--- webpack.common.js
+++ webpack.common.js
@@ -4,6 +4,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackExternalsPlugin = require('html-webpack-externals-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
 	entry: {
@@ -26,19 +27,29 @@ module.exports = {
 			cleanStaleWebpackAssets: false
 		}),
 		new CopyWebpackPlugin([
+			// {
+			// 	context: 'node_modules/cwrc-writer-base/src/css/',
+			// 	// from: '**/*',
+			// 	from: '**/*',
+			// 	to: 'css'
+			// },
+			// {
+			// 	context: 'node_modules/cwrc-writer-base/src/css/',
+			// 	from: '**/*',
+			// 	// from: '*.css',
+			// 	to: 'css'
+			// },
 			{
-				context: 'node_modules/cwrc-writer-base/build/css/',
-				from: '**/*',
-				to: 'css'
-			},{
 				context: 'node_modules/cwrc-writer-base/src/img/',
 				from: '**/*',
 				to: 'img'
-			},{
+			},
+			{
 				context: 'node_modules/cwrc-git-dialogs/src/css/',
 				from: 'bootstrap.css',
 				to: 'css'
-			},{
+			},
+			{
 				context: 'node_modules/bootstrap/fonts/',
 				from: '*',
 				to: 'fonts'
@@ -56,6 +67,13 @@ module.exports = {
 		new HtmlWebpackPlugin({
 			template: 'src/html/index.html',
 			inject: 'body'
+		}),
+		new MiniCssExtractPlugin({
+			filename: '[name].css',
+			chunkFilename: '[id].css',
+			moduleFilename: ({
+				name
+			}) => `${name.replace('/js/', '/css/')}.css`,
 		}),
 		new HtmlWebpackExternalsPlugin({
 			externals: [{
@@ -94,6 +112,58 @@ module.exports = {
 								'useESModules': false
 							}]
 						]
+					}
+				}]
+			},
+			{
+				test: /\.less$|css$/,
+				use: [
+					{
+						loader: 'style-loader', // creates style nodes from JS strings
+					},
+					{
+						loader: 'css-loader', // translates CSS into CommonJS
+					},
+					{
+						loader: 'less-loader', // compiles Less to CSS
+						options: {
+							lessOptions: {
+								relativeUrls: 'local', //https://github.com/webpack-contrib/less-loader/issues/109
+								globalVars: { parentId: '#cwrcWriterContainer' }
+							},
+						},
+					},
+					// {
+					// 	loader: MiniCssExtractPlugin.loader,
+					// 	options: {
+					// 		// you can specify a publicPath here
+					// 		// by default it uses publicPath in webpackOptions.output
+					// 		publicPath: '../',
+					// 		hmr: process.env.NODE_ENV === 'development',
+					// 		esModule: true,
+					// 	},
+					// },
+				],
+				
+			},
+			{
+				test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+				enforce: 'pre', // preload the jshint loader
+				use: [{
+					loader: 'url-loader',
+					options: {
+						query: { limit: 25000 }
+					}
+				}]
+			},
+			{
+				test: /\.(png|jpg|jpeg|gif|svg)$/,
+				enforce: 'pre', // preload the jshint loader
+				// exclude: /node_modules/, // exclude any and all files in the node_modules folder
+				use: [{
+					loader: 'url-loader',
+					options: {
+						query: { limit: 25000 }
 					}
 				}]
 			}
