f86d92049e3af588db3603e948f4d1fbe979ccc5
Luciano Frizzera <lucaju@me.com>
2020-05-22 20:05:31 -0400

    build(webpack): load svg using file loader instead of url-loader

--- webpack.common.js
+++ webpack.common.js
@@ -124,7 +124,7 @@ module.exports = {
 				],
 			},
 			{
-				test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+				test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
 				enforce: 'pre', // preload the jshint loader
 				use: [{
 					loader: 'url-loader',
