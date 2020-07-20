88df300cc9da562866d2759fc737537c7a1db7fb
Luciano Frizzera <lucaju@gmail.com>
2020-07-19 23:35:37 -0400

    refactor(server): es6 sintax over .mjs files

---
+++ server/dev/dev.mjs
@@ -0,0 +1,21 @@
+import webpack from 'webpack';
+import webpackDevMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
+
+import config from '../../webpack.dev.js';
+
+export const devTools = (app) => {
+	//webpack middleware and hot reload
+	config.entry.app.unshift('webpack-hot-middleware/client?reload=true&timeout=1000'); //Auto-reloading when webpack detects any changes
+	config.plugins.push(new webpack.HotModuleReplacementPlugin()); //Add HMR plugin
+	const compiler = webpack(config);
+	app.use(webpackDevMiddleware(compiler, {
+		publicPath: config.output.publicPath,
+		writeToDisk: true,
+	}));
+
+    //Enable "webpack-hot-middleware"
+	app.use(webpackHotMiddleware(compiler)); 
+
+	console.log('Dev Server is online!');
+};
---
+++ server/index.mjs
@@ -0,0 +1,8 @@
+import { app } from './server.mjs';
+const port = process.env.PORT || 3000;
+
+// isDev();
+
+app.listen(port, () => {
+	console.log(`Server started on port ${port}`);
+});
